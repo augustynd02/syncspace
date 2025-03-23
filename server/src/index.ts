4142cff6b3264d43a1da065b8b3c544dd43b443e
yurishe0 <yurishe00@gmail.com>
2025-03-23 15:18:43 +0100

    feat: switch to ES6 imports and ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,21 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import dotenv from 'dotenv';
+import express from 'express';
+import type { Request, Response } from "express";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-console.log(PORT);
+const PORT: number = parseInt(process.env.PORT || "3000", 10);
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("test");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
