faad7ad6c0e9f56d19d137eadfedf3409260831e
yurishe0 <yurishe00@gmail.com>
2025-03-23 13:37:09 +0100

    fix: properly config .env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
-import express from "express";
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+console.log(PORT);
 
 app.use(express.json());
 
