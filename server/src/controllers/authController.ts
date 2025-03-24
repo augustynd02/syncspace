48765e4f94044c13efbda7bf85212af23409dbe4
yurishe0 <yurishe00@gmail.com>
2025-03-24 16:33:13 +0100

    feat: add registration logic

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -6,7 +6,29 @@ const prisma = new PrismaClient();
 
 const authController = {
     registerUser: async (req: Request, res: Response, next: NextFunction) => {
-        res.send('register');
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+
+            const user = await prisma.user.create({
+                data: {
+                    username: req.body.username,
+                    password: req.body.password,
+                    name: req.body.name,
+                    ...(req.body.middle_name && { middle_name: req.body.middle_name }),
+                    last_name: req.body.last_name
+                }
+            })
+
+            if (!user) {
+                return next(new Error("Could not register user."))
+            }
+
+            res.status(201).json({
+                message: "User registered successfully"
+            })
+        } catch (err) {
+            next(err);
+        }
     },
 
     loginUser: async (req: Request, res: Response, next: NextFunction) => {
