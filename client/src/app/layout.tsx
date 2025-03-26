0d7408e446ff15540ee6ca93451069778d0d0b45
yurishe0 <yurishe00@gmail.com>
2025-03-26 20:39:39 +0100

    feat(font): add global primary and secondary fonts

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,21 @@ import type { Metadata } from "next";
 import "./globals.scss";
 import { ReactNode } from "react";
 
+import { Raleway, Montserrat } from 'next/font/google';
+
+const raleway = Raleway({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-primary'
+})
+
+const montserrat = Montserrat({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-secondary'
+})
+
+
 export const metadata: Metadata = {
 	title: "syncspace",
 	description: "SyncSpace is a dynamic social media platform that connects people, enabling seamless communication, sharing, and collaboration. Stay in sync with your network and engage with content like never before.",
@@ -9,7 +24,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode}) {
 	return (
-		<html lang="en">
+		<html lang="en" className={`${raleway.variable} ${montserrat.variable}`}>
 			<body>
 				{children}
 			</body>
