ffacf5ee3fcbdf31c1d912a4500126b5cc4ce5ff
yurishe0 <yurishe00@gmail.com>
2025-03-31 22:58:34 +0200

    feat(login): add a login csr component and include it in login page

---
+++ client/src/app/login/LoginForm.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import React, { useState } from 'react';
+import styles from './LoginForm.module.scss';
+
+type FormData = {
+    username: string;
+    password: string;
+}
+
+export default function LoginForm() {
+    const [formData, setFormData] = useState<FormData>({ username: '', password: '' });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        try {
+            e.preventDefault();
+
+            const response = await fetch("http://localhost:8000/api/auth/login", {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json' },
+                credentials: 'include',
+                body: JSON.stringify(formData),
+            })
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('Logged with id: ', data.user.id);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <section className={styles.loginContainer}>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" id="username" onChange={handleInputChange} />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" onChange={handleInputChange} />
+
+                <button type="submit">Login</button>
+            </form>
+        </section>
+    )
+}
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,79 +1,16 @@
-"use client"
-
-import { useState } from "react";
+import LoginForm from "./LoginForm"
+import styles from "./page.module.scss";
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = useState({ username: '', password: '' });
-    const [registerFormData, setRegisterFormData] = useState({ username: '', password: '', name: '', last_name: ''})
-
-    const handleLoginChange = (e) => {
-        const { name, value } = e.target;
-        setLoginFormData({ ...loginFormData, [name]: value })
-    }
-
-    const handleRegisterChange = (e) => {
-        const { name, value } = e.target;;
-        setRegisterFormData({ ...registerFormData, [name]: value });
-    }
-
-    const handleLoginSubmit = async (e) => {
-        try {
-            e.preventDefault();
-
-            const response = await fetch("http://localhost:8000/api/auth/login", {
-                method: 'POST',
-                headers: { 'Content-type': 'application/json' },
-                credentials: 'include',
-                body: JSON.stringify(loginFormData),
-            })
-
-            const data = await response.json();
-
-            if (response.ok) {
-                console.log('logged: ', data.user.id);
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    const handleRegisterSubmit= async (e) => {
-        try {
-            e.preventDefault();
-
-            const response = await fetch("http://localhost:8000/api/users", {
-                method: 'POST',
-                headers: { 'Content-type': 'application/json' },
-                credentials: 'include',
-                body: JSON.stringify(registerFormData),
-            })
-
-            const data = await response.json();
-
-            if (response.ok) {
-                console.log(data);
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
     return (
-        <>
-            <form action="" onSubmit={handleLoginSubmit}>
-                <input type="text" name="username" id="username" onChange={handleLoginChange} />
-                <input type="password" name="password" id="password" onChange={handleLoginChange} />
-                <button type="submit">submit</button>
-            </form>
-
-            <h2>register</h2>
-            <form action="" onSubmit={handleRegisterSubmit}>
-                <input type="text" name="username" id="username" onChange={handleRegisterChange} />
-                <input type="password" name="password" id="password" onChange={handleRegisterChange} />
-                <input type="text" name="name" id="name" onChange={handleRegisterChange} />
-                <input type="text" name="last_name" id="last_name" onChange={handleRegisterChange} />
-                <button type="submit">submit</button>
-            </form>
-        </>
+        <main className={styles.loginPageContainer}>
+            <div className={styles.welcomeContainer}>
+                <h1>syncspace</h1>
+                <p>Synchronize with those who matter and connect in a shared digital space, designed to make collaboration, interaction, and sharing a breeze.</p>
+            </div>
+            <div className={styles.loginContainer}>
+                <LoginForm />
+            </div>
+        </main>
     )
 }
