2b2da8b834114cb09df70fb3a0af4769b13680d6
yurishe0 <yurishe00@gmail.com>
2025-04-05 23:29:52 +0200

    fix(root): add class to main tag

--- client/src/app/(root)/layout.tsx
+++ client/src/app/(root)/layout.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header/Header"
 import Footer from "@/components/Footer/Footer"
+import styles from "./Root.module.scss"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Header />
-            <main>
+            <main className={styles.mainContainer}>
                 {children}
             </main>
             <Footer />
