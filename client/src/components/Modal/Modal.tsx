57e2ea0d1f89029d59d4ed42934ac46b69cda305
yurishe0 <yurishe00@gmail.com>
2025-04-11 21:50:36 +0200

    feat(components): add a modal component

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import styles from './Modal.module.scss'
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button/Button";
+
+interface ModalProps {
+    children: React.ReactNode
+    isOpen: boolean;
+    title: string;
+    onClose: () => void;
+}
+
+export default function Modal({ children, isOpen, title, onClose }: ModalProps) {
+    if (!isOpen) return null;
+
+    return (
+        <div className={styles.modalOverlay}>
+            <div className={styles.modalContainer}>
+                <div className={styles.modalHeader}>
+                    {title}
+                    <button onClick={onClose}>
+                        <IoMdClose />
+                    </button>
+                </div>
+                <div className={styles.modalContent}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
