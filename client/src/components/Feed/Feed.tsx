e11b0b9afb4f3fb105f7500d01527e2c34a0a297
yurishe0 <yurishe00@gmail.com>
2025-04-13 15:43:06 +0200

    feat(feed): display appropriate information when no posts are found

--- client/src/components/Feed/Feed.tsx
+++ client/src/components/Feed/Feed.tsx
@@ -1,10 +1,17 @@
 import type PostType from "@/types/Post";
 import styles from './Feed.module.scss';
 import Post from '@/components/Post/Post';
+import { PiEmpty } from "react-icons/pi";
 
 export default function Feed({ posts }: { posts: PostType[] }) {
     return (
         <section className={styles.feed}>
+            { posts.length === 0 && (
+                <div className={styles.noPosts}>
+                    <PiEmpty />
+                    <p>No posts found...</p>
+                </div>
+            )}
             { posts.map(post => {
                 return <Post key={post.id} post={post} />
             }) }
