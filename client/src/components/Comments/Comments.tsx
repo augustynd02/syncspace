2863fd58da27bdc4bbaaae7e3ba998a7e788605b
yurishe0 <yurishe00@gmail.com>
2025-04-18 23:15:08 +0200

    feat(comments): implement adding comments

--- client/src/components/Comments/Comments.tsx
+++ client/src/components/Comments/Comments.tsx
@@ -4,10 +4,58 @@ import Comment from "@/types/Comment";
 import { useState, useEffect, useRef } from "react"
 import styles from './Comments.module.scss'
 import formatDate from "@/utils/formatDate";
+import { MdSend } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from 'react-toastify';
 
-export default function Comments({ initialComments }: { initialComments: Comment[] }) {
+const createComment = async ({ commentMessage, contentType, contentId }: { commentMessage: string, contentType: 'post' | 'comment', contentId: string }) => {
+    try {
+        const response = await fetch(`http://localhost:8000/api/posts/${contentId}/comments`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                commentMessage,
+                contentType,
+                contentId
+            })
+        })
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || "Could not create post.");
+        }
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export default function Comments({ initialComments, postId }: { initialComments: Comment[], postId: string }) {
     const [comments, setComments] = useState(initialComments);
+    const [newComment, setNewComment] = useState('');
     const [expanded, setExpanded] = useState(false);
+
+    const mutation = useMutation({
+        mutationFn: createComment,
+        onSuccess: () => {
+            toast.success('Post successfully created!');
+        },
+        onError: (err) => {
+            toast.error(err.message || 'Could not create post.');
+        }
+    })
+
+    const handleSubmitComment = () => {
+        mutation.mutate({
+            commentMessage: newComment,
+            contentType: 'post',
+            contentId: postId
+        })
+    }
 
     return (
         <>
@@ -35,6 +83,19 @@ export default function Comments({ initialComments }: { initialComments: Comment
                             </article>
                         )
                     })}
+                    <div className={styles.inputContainer}>
+                        <input
+                            type="text"
+                            name="comment"
+                            id="comment"
+                            value={newComment}
+                            onChange={(e) => setNewComment(e.target.value)}
+                            placeholder='Write a new comment'
+                        />
+                        <button
+                            onClick={handleSubmitComment}
+                        ><MdSend /></button>
+                    </div>
                 </div>
             )}
         </>
