cdbd0f297e9469f09032726718fdc5b69a30a4aa
yurishe0 <yurishe00@gmail.com>
2025-04-24 10:10:11 +0200

    fix(auth): make cookie partitioned

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -75,6 +75,7 @@ const authController = {
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 secure: process.env.NODE_ENV === 'production',
                 path: '/',
+                partitioned: true
             });
             res.status(200).json({ message: 'Login successful', user: userWithoutPassword})
         } catch (err) {
