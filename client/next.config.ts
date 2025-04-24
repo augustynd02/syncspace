139a38107a1878a3cf5e56507bfcdd708f9b045b
yurishe0 <yurishe00@gmail.com>
2025-04-24 13:50:20 +0200

    fix(auth): use a reverse proxy

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['syncspace-images.s3.eu-central-1.amazonaws.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
